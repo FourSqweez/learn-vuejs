b5fbd2366b85d0517762ec907a29e6cc73171e41
FourSqweez <Kjeerasak@kkumail.com>
2021-02-19 22:17:38 +0700

    Added : Mouse Event Handling

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const Books = {
+const App = {
   data() {
     return {
       showBooks: true,
       title: 'The final empire',
       author: 'Brandon Sanderson',
       age: '40',
+      x: 0,
+      y: 0,
     }
   },
   methods: {
@@ -12,10 +14,18 @@ const Books = {
       //this.title = 'new title'
       this.title = title
     },
-    toggleShowBooks(){
+    toggleShowBooks() {
       this.showBooks = !this.showBooks
-    }
+    },
+    handleMouseEvent(e, data) {
+      console.log(e, e.type)
+      data && console.log(data)
+    },
+    handleMouseMove(e) {
+      this.x = e.offsetX
+      this.y = e.offsetY
+    },
   },
 }
 
-Vue.createApp(Books).mount('#books')
+Vue.createApp(App).mount('#app')
