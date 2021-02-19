83ae17478dfeb0edf6f0fc2c36c0d5f453f2dc70
FourSqweez <Kjeerasak@kkumail.com>
2021-02-19 23:02:13 +0700

    Added : Dynamic class

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const App = {
       url: 'http://www.google.com',
       showBooks: true,
       books: [
-        { title: 'name of the wind', author: 'patrick rothfuss' },
-        { title: 'The final empire', author: 'Brandon Sanderson' },
-        { title: 'the way kings', author: 'Brandon Sanderson' },
+        { title: 'name of the wind', author: 'patrick rothfuss',img: 'assets/1.jpg',isFav: true },
+        { title: 'The final empire', author: 'Brandon Sanderson',img: 'assets/2.jpg',isFav: false },
+        { title: 'the way kings', author: 'Brandon Sanderson',img: 'assets/3.jpg',isFav: true },
       ],
       x: 0,
       y: 0,
